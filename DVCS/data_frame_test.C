4d055863c89edd626630e40c6fa0ab23d2de62c5
johnny8266 <johnny060615@gmail.com>
2021-01-08 13:02:38 +0100

    modified the read

--- DVCS/data_frame_test.C
+++ DVCS/data_frame_test.C
@@ -1,21 +1,22 @@
 #include <ROOT/RDataFrame.hxx>
 #include <iostream>
+
 using namespace std;
-//using namespace ROOT::Experimental;
+using vectorD = vector<double>;
 
 void data_frame_test()
 {
+  ROOT::RDataFrame TDF("events", "../build/g4e_output_3000events.root", {"event_id"});
+ 
+  //  auto cutQ2 = [](double Q2) { return Q2 < 5.; };
+  auto cutQ2 = [](ULong64_t event_id) { return event_id > 5.; };
+  auto n_cut = [](const vectorD &hit_x) { return hit_x.size() > 1000; };
 
-  //  TFile *file = TFile::Open("../build/g4e_output_3000events.root");
-  //  TTree *events = (TTree *) file->Get("events");
-
-  //  ROOT::RDataFrame TDF("events", "../build/g4e_output_3000events.root");
-  ROOT::RDataFrame TDF("DVCS", "result_foam.root", {"Q2"});
-
-  auto cutQ2 = [](double Q2) { return Q2 < 5.; };
+  auto nentries = TDF.Filter(n_cut, {"hit_x"}).Count();
+  cout << *nentries << " passed all filters" << endl;
   
-  auto entries2 = TDF.Filter("Q2 < 10.").Count();
-  cout << *entries2 << endl;
+  auto entries2 = TDF.Filter("event_id > 10.").Count();
+  cout << *entries2 << " event_id > 10" << endl;
   
   //  auto h = TDF.Histo1D("Q2");
   auto hist = TDF.Filter(cutQ2).Histo1D();

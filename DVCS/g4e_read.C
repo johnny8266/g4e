c43163e3e637d91fb681da9b2f69e52dd24f9ed1
johnny8266 <johnny060615@gmail.com>
2021-01-13 00:26:16 +0100

    Work home 12012021

--- DVCS/g4e_read.C
+++ DVCS/g4e_read.C
@@ -18,17 +18,10 @@
 void g4e_read()
 {
 
-  TFile *file = TFile::Open("../build/g4e_output_3000events.root");
+  //  TFile *file = TFile::Open("../build/g4e_output_10k_events.root");
+  TFile *file = TFile::Open("../build/g4e_output.root");
   TTree *events = (TTree *) file->Get("events");
 
-  //  auto testcut = [](double x){return x>0.;};
-  //  auto testcut = [](ULong64_t x){return x>10;};
-  ROOT::RDataFrame TDF("events", file);
-  //  TDataFrame TDF("events", file);
-  auto h1 = TDF.Filter("hit_count > 100").Histo1D("hit_x");
-
-  h1->DrawCopy();
-  /*
   TTreeReader fReader("events", file);
 
   // Readers to access the data (delete the ones you do not need).
@@ -39,7 +32,7 @@ void g4e_read()
   TTreeReaderArray<unsigned long> hit_id = {fReader, "hit_id"};
   TTreeReaderArray<unsigned long> hit_trk_id = {fReader, "hit_trk_id"};
   TTreeReaderArray<unsigned long> hit_ptr_id = {fReader, "hit_ptr_id"};
-  TTreeReaderArray<unsigned long> hit_parent_trk_id = {fReader, "hit_parent_trk_id"};
+  TTreeReaderArray<unsigned long> hit_parent_trk_id = {fReader, "hit_parent_trk_id"};  // PDG code of that particle
   TTreeReaderArray<string>        hit_vol_name = {fReader, "hit_vol_name"};
   TTreeReaderArray<double>        hit_x = {fReader, "hit_x"};
   TTreeReaderArray<double>        hit_y = {fReader, "hit_y"};
@@ -92,11 +85,14 @@ void g4e_read()
   // =================================
   // Histgram
   // =================================
-  
+
+  auto electron_DPx_primary_recontruct = new TH1F("electron_DPx_primary_recontruct", "electron_DPx_primary_recontruct", 200, -0.1, 0.1);
+  auto electron_DPz_primary_recontruct = new TH1F("electron_DPz_primary_recontruct", "electron_DPz_primary_recontruct", 200, -0.1, 0.1);
+  auto photon_DPx_primary_recontruct = new TH1F("photon_DPx_primary_recontruct", "photon_DPx_primary_recontruct", 200, -0.1, 0.1);
+  auto photon_DPy_primary_recontruct = new TH1F("photon_DPy_primary_recontruct", "photon_DPy_primary_recontruct", 200, -0.1, 0.1);
+  auto photon_DPz_primary_recontruct = new TH1F("photon_DPz_primary_recontruct", "photon_DPz_primary_recontruct", 200, -0.1, 0.1);
   auto h1_el_e_tot = new TH1D("rec_el_e_tot", "Energy of the recoil electron", 150, 0., 15.);
-  h1_el_e_tot->GetXaxis()->SetTitle("E [GeV]");
   auto el_e_tot_roman = new TH1D("el_e_tot_roman", "el_e_tot_roman", 150, 0., 150.);
-  el_e_tot_roman->GetXaxis()->SetTitle("E [GeV]");
 
   auto h2_xy_hits_elcap = new TH2I("xy_hits_elcap", "X,Y hits in Electron Endcap EMCAL", 200, -1500, 1500, 200, -1500, 1500);
 
@@ -112,7 +108,9 @@ void g4e_read()
   
   while (fReader.Next())
     {
-
+      if(++events_numer > 6500)
+	break;
+      
       if(events_numer%100 == 0)
 	cout << "Read " << events_numer << " th events..." << endl;
       
@@ -123,62 +121,73 @@ void g4e_read()
       // Read basic values
       auto hits_count = static_cast<size_t>(*hit_count.Get());         // Number of hits
       auto tracks_count = static_cast<size_t>(*trk_count.Get());       // Number of tracks
+      //      cout << "This event has: " << tracks_count << " tracks || " << hits_count << " hits." << endl;
+
       
       // =============================
       // Primary Proton event
       // =============================
-
-      for(size_t p = 0 ; p < primary_count ; p++)  // iterate the primary event 
-	{
-	  if(gen_prt_pdg[p] == 2212)
-	    {
-	      //	      cout << gen_prt_pdg[p] << "  " << gen_prt_tot_mom[p] << "  " << gen_prt_tot_mom[p] * gen_prt_dir_x[p] << "  " << gen_prt_tot_mom[p] * gen_prt_dir_y[p] << "  " << gen_prt_tot_mom[p] * gen_prt_dir_z[p] << "  " << gen_prt_tot_e[p] << endl;
 	      
-	      for(size_t i = 0 ; i < hits_count ; i++)
-		{
-		  // This is is of a track that made this hit
-		  uint64_t parent_track_id = static_cast<uint64_t>(hit_trk_id[i]);
+      for(size_t i = 0 ; i < hits_count ; i++)
+	{
+	  // This is is of a track that made this hit
+	  uint64_t hit_track_id = static_cast<uint64_t>(hit_trk_id[i]);
 
-		  // This is a volume name of a hit
-		  std::string vol_name = static_cast<std::string>(hit_vol_name[i]);
+	  uint64_t hit_parent_track_id = static_cast<uint64_t>(hit_parent_trk_id[i]);
+	  
+	  // This is a volume name of a hit
+	  std::string vol_name = static_cast<std::string>(hit_vol_name[i]);
 		  
-		  double x = hit_x[i], y = hit_y[i], z = hit_z[i];            
-		  double e_loss = hit_e_loss[i];
+	  double x = hit_x[i], y = hit_y[i], z = hit_z[i];            
+	  double e_loss = hit_e_loss[i];
             
         
-		  // Check that the name starts with "ci_EMCAL"
-		  if(vol_name.rfind("ci_EMCAL", 0) == 0)
-		    {
-		      track_ids_in_ecap_emcal.insert(parent_track_id);
-		      h2_xy_hits_elcap->Fill(x, y);
-		    }
-		  // 
-		  if(vol_name.rfind("ffi", 0) == 0)
-		    {
-		      //		      track_ids_in_ffi_RPOTS.insert(parent_track_id);
-		      //		      if(trk_parent_id[i] != 0) continue;
-		      //		      h2_xy_hits_ffi_OFFM->Fill(x, y, z);
-		      //	      cout << z << endl;
-		      for(int tc = 0 ; tc < track_count ; tc++)
-		      
-		    }
-		}
+	  // Check that the name starts with "ci_EMCAL"
+	  //	  if(vol_name.rfind("ci_EMCAL", 0) == 0)
+	  if( (vol_name.rfind("cb_", 0) == 0) || (vol_name.rfind("ce_", 0) == 0) )
+	    {
+	      track_ids_in_ecap_emcal.insert(hit_track_id);
+	      //	      cout << i << " || ";
+	      //	      cout << hit_track_id << " || " << hit_parent_track_id <<  endl;
+	      //	      h2_xy_hits_elcap->Fill(x, y);
 	    }
+		  
+	  
+	    if(vol_name.rfind("ffi_RPOT", 0) == 0)
+	    {
+	      track_ids_in_ffi_RPOTS.insert(hit_track_id);
+	      //	      if(trk_parent_id[i] != 0) continue;
+	      //	      h2_xy_hits_ffi_OFFM->Fill(x, y, z);
+	      cout << trk_pdg[i] << "  " << trk_parent_id[i] << "  " << vol_name << "  " << z << endl;
+	    }
+	    
 	}
       
       // =============================
       // Iterate over tracks
       // =============================
       
-      double px, py, pz;
+      double px, py, pz, epx0, epy0, epz0, gpx0, gpy0, gpz0;
       TLorentzVector lv;
+      
+      epx0 = gen_prt_dir_x[0] * gen_prt_tot_mom[0];
+      epy0 = gen_prt_dir_y[0] * gen_prt_tot_mom[0];
+      epz0 = gen_prt_dir_z[0] * gen_prt_tot_mom[0];
+      
+      gpx0 = gen_prt_dir_x[1] * gen_prt_tot_mom[1];
+      gpy0 = gen_prt_dir_y[1] * gen_prt_tot_mom[1];
+      gpz0 = gen_prt_dir_z[1] * gen_prt_tot_mom[1];
+      	    
+      //      cout << px0 << " " << py0 << " " << pz0 << endl;
+
       for(size_t i = 0 ; i < tracks_count ; i++)
 	{
 	  //	  if(trk_pdg[i] != 11) continue;       // Take only electrons for now
+	  //	  if(trk_pdg[i] != 22) continue;       // Take only photons for now
 	  if(trk_parent_id[i] != 0) continue;  // Take only particles from a generator
                 
 	  // Check track has hits in ce_EMCAL
-	  if( track_ids_in_ecap_emcal.count(trk_id[i]) )
+	  if( track_ids_in_ecap_emcal.count( trk_id[i]) )
 	    {
 	      //continue;
 	      //	      cout << "track in the ce_EMCAL, particle: " << trk_pdg[i] << endl;
@@ -189,30 +198,54 @@ void g4e_read()
 
 	      lv.SetXYZM(px, py, pz, mass_electron);
 	      h1_el_e_tot->Fill(lv.Energy());
-	    }
 
-	  
-	  // Check track has hits in Roman Pots
-	  if( track_ids_in_ffi_RPOTS.count(trk_id[i]) )
-	    {
-	      //continue;
-	      //	      cout << "track in the far forward region, particle: " << trk_pdg[i] << endl;
-	      // Construct TLorenz vector
-	      px = trk_vtx_dir_x[i] * trk_mom[i];
-	      py = trk_vtx_dir_y[i] * trk_mom[i];
-	      pz = trk_vtx_dir_z[i] * trk_mom[i];
-
-	      lv.SetXYZM(px, py, pz, mass_electron);
-	      el_e_tot_roman->Fill(lv.Energy());
+	      if(trk_pdg[i] == 11)
+		{
+		  electron_DPx_primary_recontruct->Fill(epx0 - (lv.Px()));
+		  electron_DPz_primary_recontruct->Fill(epz0 - (lv.Pz()));
+		}
+	      if(trk_pdg[i] == 22)
+		{
+		  photon_DPx_primary_recontruct->Fill(gpx0 - (lv.Px()));
+		  photon_DPy_primary_recontruct->Fill(gpy0 - (lv.Py()));
+		  photon_DPz_primary_recontruct->Fill(gpz0 - (lv.Pz()));
+		}
+	      
+	      //	      cout << epx0 - (lv.Px()) << "  " << epy0 - (lv.Py()) << "  " << epz0 - (lv.Pz()) << "  ";
+	      //	      cout << trk_id[i] << endl;
 	    }
-	  
 	}
     }
-  */
+
+
+  
+  // ======================================
+  // Draw the Results
+  // ======================================
+  
   //  auto c1 = new TCanvas("c1","The Canvas Title",800,800);
   //  h1_el_e_tot->Draw();
   //  el_e_tot_roman->Draw();
   
-  auto c2 = new TCanvas("c2","The Canvas Title",800,800);
+  auto c2 = new TCanvas("c2","The Canvas Title", 1200, 800);
+  c2->Divide(3,2);
+
+  c2->cd(1);
+  photon_DPx_primary_recontruct->GetXaxis()->SetTitle("PX0 - PX1");
+  photon_DPx_primary_recontruct->Draw();
+  c2->cd(2);
+  photon_DPy_primary_recontruct->GetXaxis()->SetTitle("PY0 - PY1");
+  photon_DPy_primary_recontruct->Draw();
+  c2->cd(3);
+  photon_DPz_primary_recontruct->GetXaxis()->SetTitle("PZ0 - PZ1");
+  photon_DPz_primary_recontruct->Draw();
+  c2->cd(4);
+  electron_DPx_primary_recontruct->GetXaxis()->SetTitle("PX0 - PX1");
+  electron_DPx_primary_recontruct->Draw();
+  c2->cd(6);
+  electron_DPz_primary_recontruct->GetXaxis()->SetTitle("PZ0 - PZ1");
+  electron_DPz_primary_recontruct->Draw();
+
+
   //  h2_xy_hits_elcap->Draw("colorz");
 }
